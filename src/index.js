1d46e4f94e3d438e4c6e0bd505d1d5c849038f17
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2020-10-02 16:11:49 +0300

    init: start&end typycal arrays problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = min = (array) => {
+    return !Array.isArray(array) || array.length === 0 ? 0 : Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = max = (array) => {
+    return !Array.isArray(array) || array.length === 0 ? 0 : Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = avg = (array) => {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        array.forEach((el) => {
+            sum += el;
+        });
+        return sum / array.length;
+    }
+};
